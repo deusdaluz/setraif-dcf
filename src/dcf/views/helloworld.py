725b56f8738ef36cee42ed84a71fb97bf580415e
ruback <ruback@gmail.com>
2012-11-11 00:12:12 -0200

    Filtros no banco

--- src/dcf/views/helloworld.py
+++ src/dcf/views/helloworld.py
@@ -1,5 +1,8 @@
 import flask
 import json
+from dcf.models.transacao import Transacao
+from flask import render_template
+from datetime import datetime
 
 bp = flask.Blueprint("helloworld", __name__)
 
@@ -10,3 +13,37 @@ def message():
         "idTransaction":"1",
         "isFraud":"false" if "legitima" in flask.request.args else "true"
     })
+
+
+
+
+@bp.route("/relatorio")
+def relatorio():
+    transacoes = Transacao.query().fetch()
+    args = flask.request.args
+
+    query = Transacao.query()
+    for key in args:
+        if args[key] != '':
+            if key == 'ehFraude':
+                query = query.filter(Transacao.ehFraude == convert(args['ehFraude']))
+            elif key == 'dataInic':
+                query = query.filter(Transacao.data >= datetime.strptime(args[key], '%Y-%m-%d'))
+            elif key == 'dataFim':
+                query =  query = query.filter(Transacao.data < datetime.strptime(args[key], '%Y-%m-%d'))
+            else:
+                query = query.filter(getattr(Transacao,key) == args[key])
+
+            transacoes = query.fetch()
+
+    return render_template('list_transacoes.html', transacoes = transacoes , args = args)
+
+@bp.route("/about")
+def about():
+    return render_template('about.html')
+
+def convert(string_bool):
+    if 'False' == string_bool:
+        return False
+    else:
+        return True
