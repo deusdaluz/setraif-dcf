569d3c9dca24d0ed64b411c2b7abbefbcb755595
Walter Cacau <waltercacau@gmail.com>
2012-11-12 10:41:33 -0200

    Adding a test interface

--- src/dcf/views/helloworld.py
+++ src/dcf/views/helloworld.py
@@ -3,18 +3,24 @@ import json
 from dcf.models.transacao import Transacao
 from flask import render_template
 from datetime import datetime
+from werkzeug.exceptions import BadRequest
 
 bp = flask.Blueprint("helloworld", __name__)
 
 
 @bp.route("/")
+def index():
+    return render_template('index.html')
+
+
+@bp.route("/checar", methods=["POST"])
 def message():
+    if flask.request.json is None:
+        raise BadRequest()
     return json.dumps({
-        "idTransaction":"1",
-        "isFraud":"false" if "legitima" in flask.request.args else "true"
+        "idTransaction": "1",
+        "isFraud": "false"
     })
-
-
 
 
 @bp.route("/relatorio")
@@ -38,9 +44,11 @@ def relatorio():
 
     return render_template('list_transacoes.html', transacoes = transacoes , args = args)
 
+
 @bp.route("/about")
 def about():
     return render_template('about.html')
+
 
 def convert(string_bool):
     if 'False' == string_bool:
