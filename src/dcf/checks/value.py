8cabece3a2462c04fef79048aa8ecca641517b65
ruback <ruback@gmail.com>
2012-11-14 17:56:47 -0200

    Fraude por valor implementada

--- src/dcf/checks/value.py
+++ src/dcf/checks/value.py
@@ -1,8 +1,24 @@
-
+import google.appengine.ext.ndb as ndb
+from dcf.models.transacao import Transacao
 
 def is_fraud(transacao, conta):
+    max = get_max_value(transacao, conta)
+
+    if ((transacao.valor - max) > 1000):
+    	if (transacao.valor > 1.8*max):
+    		return True
+
     return False
 
 
 def learn(transacao, conta):
     pass
+
+def get_max_value(transacao, conta):
+	key = ndb.Key('Conta', transacao.idConta)
+	transacoes = Transacao.query(ancestor = key).filter(Transacao.ehFraude == False).order(-Transacao.valor)
+	transacao_max = transacoes.get()
+	if (transacao_max is None):
+		return 0
+	else:
+		return transacao_max.get()
